b652653f9474074e02d3c8a4656d61bcb7099f1a
unknown <sectumsempra82@gmail.com>
2021-09-13 18:40:11 +0200

    refactoring searchbar to functional component

--- src/components/index/home.js
+++ src/components/index/home.js
@@ -1,14 +1,13 @@
 import React, { Fragment } from 'react';
 
-class Homepage extends React.Component {
-    render(){
+export default function Homepage(props) {
+
         return(
             <Fragment>
                 <h1>Welcome to Pokemon React calculator</h1>
                 <h3>The aim of this tool is to calculate various stats of a Pokemon at a given level, further implementations might add other features</h3> 
             </Fragment>
         )
-    }
 
 }
 
