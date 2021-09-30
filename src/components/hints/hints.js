c8fd16422e3ebf68585a5f99c1cc959466c92893
unknown <sectumsempra82@gmail.com>
2021-09-30 21:19:16 +0200

    created selectors and cleaned Hints hook

--- src/components/hints/hints.js
+++ src/components/hints/hints.js
@@ -1,6 +1,6 @@
 
-import { useEffect, useState } from "react";
-import {axiosCached} from '../AxiosCached/AxiosCached'
+import { useEffect, useState, useCallback } from "react";
+import { axiosCached } from '../AxiosCached/AxiosCached'
 
 
 
@@ -11,22 +11,27 @@ export default function useHints(inputValue) {
   const [pkmList, setPkmList] = useState([]);
   const [hints, setHints] = useState([]);
 
+  const getFilteredHints = useCallback((name) => {
+    var hintsFiltered = pkmList.filter(pkm => pkm.includes(name));
+    return(hintsFiltered);
+  });
+
+  //this useEffect actually runs every time we change input in order to filter the cached list as desired
+
   useEffect(() => {
 
-
-    const getHints = ( name ) => {
-      var hintsFiltered = pkmList.filter(pkm => pkm.includes(name));
-      setHints(hintsFiltered);
-    }
-    
-    if(inputValue && inputValue.length > 2 ){
-      getHints(inputValue);
+    if (inputValue && inputValue.length > 2) {
+      setHints(getFilteredHints(inputValue));
     } else {
       console.log('name too short');
       setHints([]);
     }
-  
+
   }, [inputValue, pkmList])
+
+
+  // We retrieve the full list only the first time and we save it in local state
+  // no need of keeping this in global state
 
   useEffect(() => {
     const headers = {
@@ -35,7 +40,7 @@ export default function useHints(inputValue) {
       'Access-Control-Allow-Origin': '*',
     }
     axiosCached
-    .post('https://beta.pokeapi.co/graphql/v1beta', {
+      .post('https://beta.pokeapi.co/graphql/v1beta', {
 
         query: `
           query pokeQuery {
@@ -45,25 +50,26 @@ export default function useHints(inputValue) {
             }
         }
           `
-          
-      
-    },
-    {crossdomain: true},
-    {headers: headers}
-  )
-    .then((response) => {
-      console.log(response.data.data.pokemon_v2_pokemon);
-      console.log(response.request.fromCache ? "Cached!" : "Not cached :(")
-      if(response.data.data.pokemon_v2_pokemon){
-        var list = response.data.data.pokemon_v2_pokemon.map(pkm => pkm.name);
-        setPkmList(list);
-     }})
-    .catch(err => {
-      console.log('error fetching hints')
-      setPkmList([]);
-    })
-    ;
+
+
+      },
+        { crossdomain: true },
+        { headers: headers }
+      )
+      .then((response) => {
+        console.log(response.data.data.pokemon_v2_pokemon);
+        console.log(response.request.fromCache ? "Cached!" : "Not cached :(")
+        if (response.data.data.pokemon_v2_pokemon) {
+          var list = response.data.data.pokemon_v2_pokemon.map(pkm => pkm.name);
+          setPkmList(list);
+        }
+      })
+      .catch(err => {
+        console.log('error fetching hints')
+        setPkmList([]);
+      })
+      ;
   }, [])
 
-  return (hints); 
+  return (hints);
 }
